92fac98a4f02ef10e4904116357b5e72eee62051
zanton173 <zanton173@gmail.com>
2023-09-16 12:56:03 -0400

    Added 404 page and web server code

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
 
+func main() {
+	h1 := func(w http.ResponseWriter, r *http.Request) {
+
+		switch r.URL.Path {
+		case "/home":
+			tmpl := template.Must(template.ParseFiles("./index.html"))
+			tmpl.Execute(w, nil)
+
+		case "/":
+			tmpl := template.Must(template.ParseFiles("./404.html"))
+			tmpl.Execute(w, nil)
+		}
+
+	}
+
+	http.HandleFunc("/", h1)
+	log.Fatal(http.ListenAndServe(":8040", nil))
 }

e5dd5de04a28cc8fa6cb09f2ba46c7e93b994430
zanton173 <zanton173@gmail.com>
2023-09-18 11:03:29 -0400

    Removed certs & added redirects for unknown routes

--- main.go
+++ main.go
@@ -7,21 +7,20 @@ import (
 )
 
 func main() {
-	h1 := func(w http.ResponseWriter, r *http.Request) {
+
+	indexHandler := func(w http.ResponseWriter, r *http.Request) {
 		///http.Handle("/images/", http.FileServer(http.Dir("images")))
 		switch r.URL.Path {
 		case "/home":
-			tmpl := template.Must(template.ParseFiles("./index.html"))
+			tmpl := template.Must(template.ParseFiles("index.html"))
 			tmpl.Execute(w, nil)
-
-		case "/":
-			tmpl := template.Must(template.ParseFiles("./404.html"))
-			tmpl.Execute(w, nil)
+		default:
+			http.Redirect(w, r, "/home", 301)
 		}
 
 	}
 
-	http.HandleFunc("/", h1)
+	http.HandleFunc("/", indexHandler)
 	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
-	log.Fatal(http.ListenAndServe(":8040", nil))
+	log.Fatal(http.ListenAndServe(":83", nil))
 }

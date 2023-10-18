5578e01f42a9457fed1bc34cc2feb693e62a1aef
zanton173 <zanton173@gmail.com>
2023-10-18 17:40:13 -0400

    Testing push in https

--- js/worker.js
+++ js/worker.js
@@ -4,21 +4,22 @@ onmessage = (event) => {
     const result = event.data
     postMessage(result)
 }
-self.addEventListener("push", (event) => {
+this.addEventListener("push", (event) => {
     const notification = event.data.json();
+
     // {"title":"Hi" , "body":"something amazing!" , "url":"./?message=123"}
-    console.log(event)
-    event.waitUntil(self.registration.showNotification(notification.title, {
+    console.log(notification)
+    const notify = new Notification("testtiele")
+    event.waitUntil(this.registration.showNotification(notification.title, {
         body: notification.body,
 
     }));
+    notify.onshow = (event) => {
+        console.log(event)
+    }
 });
-
-self.addEventListener("notificationclick", (event) => {
-    event.waitUntil(clients.openWindow(event.notification.data.notifURL));
-});
-/*navigator.serviceWorker.getRegistration().then((data) => data.pushManager.subscribe({
+/*
+navigator.serviceWorker.getRegistration().then((data) => data.pushManager.subscribe({
     userVisibleOnly: true
 }).then(resp => console.log(resp)))*/
 
-//navigator.serviceWorker.getRegistration("localhost").then

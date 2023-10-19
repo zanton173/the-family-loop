dda7a25e0f41844ea1c80a12c6e07986f0fb12ca
zanton173 <zanton173@gmail.com>
2023-10-19 06:04:52 -0400

    Notification for new posts added

--- js/worker.js
+++ js/worker.js
@@ -1,14 +1,16 @@
-
 onmessage = (event) => {
 
     const result = event.data
-    console.log(result)
+
     if (result[0] !== result[1] && result[0] != null) {
         new Notification("Someone just made a new post!", {
             body: "There's a new post!",
+            icon: "../assets/android-chrome-512x512.png",
+            tag: "newPostTag" + result[0]
         });
     }
 }
+
 this.addEventListener("push", (event) => {
     const notification = event.data.json();
 

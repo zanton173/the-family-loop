5bc212a67c32e2324a6d25fcba96a410ed952840
zanton173 <zanton173@gmail.com>
2023-10-22 07:09:20 -0400

    Removed test

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,22 +3,10 @@ importScripts('https://www.gstatic.com/firebasejs/10.5.0/firebase-messaging.js')
 this.addEventListener("push", (event) => {
 
     const notification = event.data.json().notification
-    console.log(event.data.json())
+
     event.waitUntil(this.registration.showNotification(notification.title, {
         body: notification.body,
         icon: "assets/apple-touch-icon.png"
     }));
 
-});
-this.addEventListener("message", ({ data }) => {
-    if (data.action === "initializeFirebase") {
-        if (firebase.messaging.isSupported()) {
-            const { config } = data.payload;
-
-            firebase.initializeApp(config);
-            firebase.messaging();
-        } else {
-            alert("not supported")
-        }
-    }
 });

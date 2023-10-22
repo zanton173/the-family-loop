4e7b0de016f98265db8c0e3c5b1214f865333196
zanton173 <zanton173@gmail.com>
2023-10-22 07:03:06 -0400

    Test service worker init

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,3 +1,5 @@
+importScripts('https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/10.5.0/firebase-messaging.js');
 this.addEventListener("push", (event) => {
 
     const notification = event.data.json().notification
@@ -7,4 +9,16 @@ this.addEventListener("push", (event) => {
         icon: "assets/apple-touch-icon.png"
     }));
 
+});
+this.addEventListener("message", ({ data }) => {
+    if (data.action === "initializeFirebase") {
+        if (firebase.messaging.isSupported()) {
+            const { config } = data.payload;
+
+            firebase.initializeApp(config);
+            firebase.messaging();
+        } else {
+            alert("not supported")
+        }
+    }
 });

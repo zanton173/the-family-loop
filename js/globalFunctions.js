f4d4cb5705db293f2d9f763398a1a876abc867a5
zanton173 <zanton173@gmail.com>
2023-12-28 07:54:16 -0500

    Notification on user gesture for iPhones

--- js/globalFunctions.js
+++ js/globalFunctions.js
@@ -10,3 +10,38 @@ function logoutFunction() {
     //window.location.reload()
 
 }
+
+async function getNotified() {
+    !("Notification" in window)
+        ? alert("This browser does not support desktop notification")
+        : await Notification.requestPermission().then(() => {
+            if (Notification.permission == 'granted') {
+
+                getToken(messaging, { vapidKey: "BJmKY269Mkqw_zRnXy0n1ncFOBsamgi7hSpli4hKGlAJ-OKTae7qj8scasqrO9dpdmntNXXgbsMK3okY0bpOBVQ" })
+                    .then((currentToken) => {
+
+                        notificationBody = {
+                            fcm_token: currentToken
+                        }
+                        fetch("/create-subscription", {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json"
+                            },
+                            body: JSON.stringify(notificationBody)
+                        })
+                            .then(() => {
+                                return window.navigator.serviceWorker
+                                    .getRegistration('/firebase-cloud-messaging-push-scope')
+                                    .then((serviceWorker) => {
+                                        if (serviceWorker) return serviceWorker;
+                                        return window.navigator.serviceWorker.register('/firebase-messaging-sw.js', {
+                                            scope: '/firebase-cloud-messaging-push-scope',
+                                        });
+                                    });
+                            })
+                    })
+            }
+        }).catch((err) => alert(err))
+
+}
